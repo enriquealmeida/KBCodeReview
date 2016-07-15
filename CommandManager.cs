ea6077e24348b9133d1fb7f316d9d8da3e479035
jlr <jlr@843fd666-81f2-4710-8602-7759cdb5ad05>
2016-07-15 23:23:33 +0000

    Renombro query handlers de los menúes a EnableAlways y EnableWhenKbOpened Arreglo para usar el que corresponde a cada caso (todos usaban el que habilita siempre) Saco usings innecesarios y otros cambios menores

--- CommandManager.cs
+++ CommandManager.cs
@@ -1,88 +1,73 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Reflection;
-using System.Text;
-using System.Threading;
-using System.Windows.Forms;
-using System.Xml;
-
 using Artech.Architecture.UI.Framework.Helper;
 using Artech.Architecture.UI.Framework.Services;
 using Artech.Common.Framework.Commands;
-using Artech.Architecture.Common.Objects;
-using Artech.Common.Framework.Selection;
 using System.Diagnostics;
+using System.Reflection;
+using System.Windows.Forms;
 
 namespace GUG.Packages.KBCodeRevisor
 {
-    class CommandManager : CommandDelegator
-    {
-        public CommandManager()
-        {
-            AddCommand(CommandKeys.ObjectInTextFormat, new ExecHandler(ExecObjectInTextFormat), new QueryHandler(QueryKBCodeRevisorNoKB));
-            AddCommand(CommandKeys.OpenFolderKBCodeRevisor, new ExecHandler(ExecOpenFolderKBCodeRevisor), new QueryHandler(QueryKBCodeRevisorNoKB));
-            AddCommand(CommandKeys.AboutKBCodeRevisor, new ExecHandler(ExecAboutKBCodeRevisor), new QueryHandler(QueryKBCodeRevisorNoKB));
-            AddCommand(CommandKeys.HelpKBCodeRevisor, new ExecHandler(ExecHelpKBCodeRevisor), new QueryHandler(QueryKBCodeRevisorNoKB));
-        }
+	class CommandManager : CommandDelegator
+	{
+		public CommandManager()
+		{
+			AddCommand(CommandKeys.ObjectInTextFormat, new ExecHandler(ExecObjectInTextFormat), new QueryHandler(EnableWhenKbOpened));
+			AddCommand(CommandKeys.OpenFolderKBCodeRevisor, new ExecHandler(ExecOpenFolderKBCodeRevisor), new QueryHandler(EnableWhenKbOpened));
+			AddCommand(CommandKeys.AboutKBCodeRevisor, new ExecHandler(ExecAboutKBCodeRevisor), new QueryHandler(EnableAlways));
+			AddCommand(CommandKeys.HelpKBCodeRevisor, new ExecHandler(ExecHelpKBCodeRevisor), new QueryHandler(EnableAlways));
+		}
 
-      public bool ExecAboutKBCodeRevisor(CommandData cmdData)
-        {
-            Assembly assem = this.GetType().Assembly;
-            object[] atributos = assem.GetCustomAttributes(typeof(AssemblyVersionAttribute), false);
-            using (Form aboutBox = new AboutKBCodeRevisor())
-            {
-                aboutBox.ShowDialog();
-            }
-            return true;
-        }
+		public bool ExecAboutKBCodeRevisor(CommandData cmdData)
+		{
+			Assembly assem = this.GetType().Assembly;
+			object[] atributos = assem.GetCustomAttributes(typeof(AssemblyVersionAttribute), false);
+			using (Form aboutBox = new AboutKBCodeRevisor())
+			{
+				aboutBox.ShowDialog();
+			}
+			return true;
+		}
 
-        public bool ExecHelpKBCodeRevisor(CommandData cmdData)
-        {
-            Process.Start("http://wiki.genexus.com/commwiki/servlet/hwikibypageid?31586");
-            return true;
-        }
+		public bool ExecHelpKBCodeRevisor(CommandData cmdData)
+		{
+			Process.Start("http://wiki.genexus.com/commwiki/servlet/hwikibypageid?31586");
+			return true;
+		}
 
-        public bool ExecObjectInTextFormat(CommandData cmdData)
-        {
-            KBCodeRevisorHelper.ExportObjectInTextFormat();
-            return true;
-        }
+		public bool ExecObjectInTextFormat(CommandData cmdData)
+		{
+			KBCodeRevisorHelper.ExportObjectInTextFormat();
+			return true;
+		}
 
+		public bool ExecOpenFolderKBCodeRevisor(CommandData cmdData)
+		{
+			KBCodeRevisorHelper.OpenFolderKBCodeRevisor();
+			return true;
+		}
 
+		private bool EnableWhenKbOpened(CommandData cmdData, ref CommandStatus status)
+		{
+			// This is where you have a chance to modify the status of
+			// menu / toolbar items.
+			status.State = CommandState.Disabled;
 
-        public bool ExecOpenFolderKBCodeRevisor(CommandData cmdData)
-        {
-            KBCodeRevisorHelper.OpenFolderKBCodeRevisor();
-            return true;
-        }
+			IKBService kbserv = UIServices.KB;
+			if (kbserv != null && kbserv.CurrentKB != null)
+			{
+				status.State = CommandState.Enabled;
+			}
 
+			// return true to indicate you already resolved the command status;
+			// otherwise the framework will try with its next registered
+			// command target
+			return true;
+		}
 
-        private bool QueryKBCodeRevisor(CommandData cmdData, ref CommandStatus status)
-        {
-            // This is where you have a chance to modify the status of
-            // menu / toolbar items.
-            status.State = CommandState.Disabled;
-
-            IKBService kbserv = UIServices.KB;
-            if (kbserv != null && kbserv.CurrentKB != null)
-            {
-                status.State = CommandState.Enabled;
-            }
-
-            // return true to indicate you already resolved the command status;
-            // otherwise the framework will try with its next registered
-            // command target
-            return true;
-        }
-
-        private bool QueryKBCodeRevisorNoKB(CommandData cmdData, ref CommandStatus status)
-        {
-            status.State = CommandState.Enabled;
-
-            return true;
-        }
-    
-
-    }
+		private bool EnableAlways(CommandData cmdData, ref CommandStatus status)
+		{
+			status.State = CommandState.Enabled;
+			return true;
+		}
+	}
 }
